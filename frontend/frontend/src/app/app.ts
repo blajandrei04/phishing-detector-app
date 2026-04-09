d095ecd3bf7f2dd9d33a9d5cc7cc7ffa3e66e9f3
blajandrei04 <andrei.florin.blaj@stud.ubbcluj.ro>
2026-04-09 18:43:42 +0300

    feat(app): fixes in the frontend sidebar

--- frontend/frontend/src/app/app.ts
+++ frontend/frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SidebarComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
